3f79c9b9d939c26d5a74d0ad79843e9d2a0b5884
kat055 <gacusankatherine0514@gmail.com>
2022-03-24 14:03:30 -0700

    search

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,28 @@
 <script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
+
+
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+<div class="bg-slate-400 h-screen w-screen">
+
+<div class="text-center py-40">
+
+  <div class="text-6xl font-bold">
+    SEARCH BAR</div>
+
+  <form action="search" class="py-6">
+    <input class="w-7/12 h-10 
+    text-left px-5 rounded"
+    type="text" placeholder="SEARCH">
+  </form>
+
+  <router-link to="/about" class="font-medium text-lg px-10 py-2
+  rounded bg-purple-300">
+  GO</router-link>
+
+</div>
+
+</div>
 </template>
